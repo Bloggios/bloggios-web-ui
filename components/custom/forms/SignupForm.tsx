879bb9309a57fad3203aad382e66680eeecbd932
Rohit <rohitparih@gmail.com>
2024-05-21 23:55:23 +0530

    feature/signup-page-validations

--- components/custom/forms/SignupForm.tsx
+++ components/custom/forms/SignupForm.tsx
@@ -1,9 +1,69 @@
+"use client";
+
 import {Button, Input} from "@nextui-org/react";
-import Link from "next/link";
+import {ChangeEvent, FormEvent, useState} from "react";
+import {EyeClosedIcon, EyeOpenIcon} from "@radix-ui/react-icons";
+import {PASSWORD_REGEX} from "@/constants/ServiceConstants";
+
+interface SignupData {
+    email: String,
+    password: String,
+}
 
 export default function SignupForm() {
+
+    const [isPasswordVisible, setIsPasswordVisible] = useState<boolean>(false);
+    const [data, setData] = useState<SignupData>({
+        email: "",
+        password: ""
+    });
+    const [errorData, setErrorData] = useState<SignupData>({
+        email: "",
+        password: ""
+    })
+
+    const handleInputChange = (
+        event: ChangeEvent<HTMLInputElement>,
+        property: keyof SignupData
+    ) => {
+        setErrorData(prevState => ({
+            ...prevState,
+            [property]: ''
+        }));
+        setData(prevState => ({
+            ...prevState,
+            [property]: event.target.value
+        }))
+    }
+
+    const validatePassword = (value: String) => value.match(PASSWORD_REGEX);
+
+    const handleValidate = () => {
+        if (data.password.length === 0) {
+            setErrorData(prevState => ({
+                ...prevState,
+                password: 'Please enter a Password'
+            }));
+            return;
+        }
+        else if (!validatePassword(data.password)) {
+            setErrorData(prevState => ({
+                ...prevState,
+                password: 'Password must be 8 Characters long with at least 1 Uppercase, 1 Number and 1 Special Character'
+            }));
+            return;
+        }
+    }
+
+    const handleSubmit = (
+        event: FormEvent<HTMLFormElement>
+    ) => {
+        event.preventDefault();
+        handleValidate();
+    }
+
     return (
-        <form className={"flex flex-col gap-6"}>
+        <form onSubmit={handleSubmit} className={"flex flex-col gap-6"}>
             <Input
                 isClearable
                 type="email"
@@ -11,18 +71,35 @@ export default function SignupForm() {
                 label="Email"
                 maxLength={40}
                 required
+                onChange={(event)=> handleInputChange(event, "email")}
             />
 
             <Input
                 isClearable
-                type="password"
+                type={isPasswordVisible ? "text" : "password"}
                 variant={"bordered"}
                 label="Password"
-                maxLength={40}
+                maxLength={28}
                 required
+                isInvalid={errorData.password.length > 0}
+                errorMessage={errorData.password}
+                onChange={(event)=> handleInputChange(event, "password")}
+                endContent={
+                    <Button
+                        onClick={()=> setIsPasswordVisible(!isPasswordVisible)}
+                        isIconOnly={true}
+                        variant={"faded"}
+                        size={"sm"}
+                        className={"bg-transparent rounded-full border-none"}
+                    >
+                        {isPasswordVisible ? (
+                            <EyeClosedIcon />
+                        ) : <EyeOpenIcon />}
+                    </Button>
+                }
             />
 
-            <Button color={"primary"}>
+            <Button type={"submit"} color={"primary"}>
                 Signup
             </Button>
         </form>
---
+++ constants/ServiceConstants.jsx
@@ -0,0 +1,1 @@
+export const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
