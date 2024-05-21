230cef2ddde638deaad6cdf80044cdd9ceb773ac
Rohit <rohitparih@gmail.com>
2024-05-21 21:56:18 +0530

    feature/auth-pages

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -16,7 +16,7 @@ export const metadata : Metadata = {
         title: "Bloggios Login",
         description: "Login to Bloggios, your all-in-one platform for blogs, Q&A, posts, messaging, and more. Connect with a vibrant community, share your insights, ask questions, and stay informed. Join Bloggios today!",
         type: "website",
-        url: "https://bloggios.com/login",
+        url: "https://dev.bloggios.in/login",
         images: bgAccentOuterRounded,
     },
     twitter: {
